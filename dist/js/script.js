a02f7913396778a7b13e0ae3830cfcbcb31dc07d
“Abd <abdmalik1207@gmail.com>
2024-07-21 15:03:22 +0800

    m

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,37 @@ hamburger.addEventListener('click', function () {
  hamburger.classList.toggle('hamburger-active');
  navMenu.classList.toggle('hidden');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const introText = "Hey, I'm <b class='text-danger'>Abd Malik</b>";
+    const jobText = "Web Developer";
+    const introElement = document.getElementById("intro");
+    const jobElement = document.getElementById("job");
+
+    let introIndex = 0;
+    let jobIndex = 0;
+    let currentIntroText = "";
+    let currentJobText = "";
+
+    function typeIntro() {
+        if (introIndex < introText.length) {
+            currentIntroText += introText[introIndex] === '<' ? introText.slice(introIndex, introText.indexOf('>', introIndex) + 1) : introText[introIndex];
+            introIndex += introText[introIndex] === '<' ? introText.indexOf('>', introIndex) + 1 - introIndex : 1;
+            introElement.innerHTML = currentIntroText;
+            setTimeout(typeIntro, 100);
+        } else {
+            setTimeout(typeJob, 500); // Delay before starting the second text
+        }
+    }
+
+    function typeJob() {
+        if (jobIndex < jobText.length) {
+            currentJobText += jobText[jobIndex];
+            jobElement.innerHTML = currentJobText;
+            jobIndex++;
+            setTimeout(typeJob, 100);
+        }
+    }
+
+    typeIntro();
+});
